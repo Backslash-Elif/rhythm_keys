f9cc7259c54141a5c0986fe309831629e21f25f0
Noah Zimmermann <noah.zimmermann@bbcag.ch>
2024-09-18 16:57:37 +0200

    color unification (unfinished)

--- components/styles.py
+++ components/styles.py
@@ -1,11 +1,59 @@
-def compute_accent(original_color: tuple, lighten:bool=False): #calculates the x% lighter or darker accent of a color
-    percentage = 15 #%
-    difference_factor = 100/percentage
-    difference = (original_color[0]/difference_factor, original_color[1]/difference_factor, original_color[2]/difference_factor)
-    if lighten:
-        return (int(original_color[0]+difference[0]), int(original_color[1]+difference[1]), int(original_color[2]+difference[2])) #returns color with lighter accent
+def compute_accent(original_color: tuple) -> tuple[tuple, tuple]: #calculates the lighter or darker accent of a color
+    brightness = 0.299*original_color[0]/255 + 0.587*original_color[1]/255 + 0.114*original_color[2]/255 #0.299, 0.587, 0.114 perceived brightness of rgb
+    
+    difference = 20 #seems to be an universally good color difference
+    if brightness >= 0.5:
+        difference *= -1 #making negative for darkening
+        text_color = (0, 0, 0)
     else:
-        return (int(original_color[0]-difference[0]), int(original_color[1]-difference[1]), int(original_color[2]-difference[2])) #returns color with darker accent
+        text_color = (255, 255, 255)
+    
+    return (original_color, (min(255, max(0, original_color[0]+difference)), min(255, max(0, original_color[1]+difference)), min(255, max(0, original_color[2]+difference))), text_color) #returns both original and accent and text color
+
+colors = { #standard colors in format (R, G, B)
+    "red": (255, 0, 0),
+    "light_red": (255, 99, 71),
+    "dark_red": (139, 0, 0),
+    "green": (0, 255, 0),
+    "light_green": (0, 255, 127),
+    "darkgreen": (0, 100, 0),
+    "blue": (0, 0, 255),
+    "light_blue": (11, 87, 208),
+    "dark_blue": (0, 0, 128),
+    "purple": (132, 3, 252),
+    "pink": (255, 20, 147),
+    "magenta": (255, 0, 255),
+    "orange": (255, 165, 0),
+    "yellow": (255, 255, 0),
+    "gray": (128, 128, 128),
+    "light_gray": (200, 200, 200),
+    "dark_gray": (64, 64, 64),
+    "black_gray": (33, 33, 33),
+    "white": (255, 255, 255),
+    "black": (0, 0, 0)
+}
+
+themed_colors = {} #pre-calculated accented colors in format ((standard color), (accented color), (text color))
+for key, value in colors.items():
+    themed_colors[key] = compute_accent(value)
+
+UI_colors = {} #primary, secondary, attention, danger, success
+
+background_gradient = {#((bgcolor-start), (bgcolor-end), dark_color:bool)
+    "midnight": (colors["blue"], colors["dark_blue"], True),
+    "daybreak": (colors["orange"], colors["purple"], False),
+    "Cotton_candy": (colors["pink"], colors["purple", False]),
+    "grassy_hill": (colors["light_green"], colors["green"], False),
+    "ocean": (colors["light_blue"], colors["blue"], False),
+    "coral_reef": (colors["red"], colors["blue"], True)
+}
+
+card_themes = { #(R, G, B, A)
+    "light": colors["white"]+(64, ),
+    "dark": colors["black"]+(64, ),
+    "primary": colors["light_blue"]+(64, ),
+    "attention": colors["yellow"]+(64, ),
+}
 
 class Styles:
     class button:
